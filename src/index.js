dfa275f46df2d83bb6ddb945c5778afa117fab9e
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-04-28 23:43:58 +0200

    add css style and fixed markup

--- src/index.js
+++ src/index.js
@@ -70,24 +70,28 @@ function createMarkup({
     comments,
     downloads,
   }) {
-    return `
-    <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-        <p class="info-item">
-            <b>Likes<br>${likes}</b>
-        </p>
-        <p class="info-item">
-            <b>Views<br>${views}</b>
-        </p>
-        <p class="info-item">
-            <b>Comments<br>${comments}</b>
-        </p>
-        <p class="info-item">
-            <b>Downloads<br>${downloads}</b>
-        </p>
-    </div>
-    </div>`;
+    return `<div class="photo-card">
+  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" />
+	</a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+		${likes} 
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+		${views} 
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+		${comments} 
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+		${downloads} 
+    </p>
+  </div>
+</div>`;
 }
 
 function createGallery(markup) {
